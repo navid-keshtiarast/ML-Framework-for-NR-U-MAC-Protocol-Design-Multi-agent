0258e07281a9e03e8f7df8dcf4ebefa497ffcaa5
oliverenaldi <oliver.renaldi@rwth-aachen.de>
2024-11-21 16:49:18 +0100

    Added license

--- contrib/ai/examples/nr-ai-mac/use-gym/nr-mac-env.h
+++ contrib/ai/examples/nr-ai-mac/use-gym/nr-mac-env.h
@@ -1,6 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2018 Technische Universität Berlin
+ * Copyright (c) 2024 RWTH Aachen University
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,9 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Piotr Gawlowicz <gawlowicz@tkn.tu-berlin.de>
- * Modify: Pengyu Liu <eic_lpy@hust.edu.cn> 
- *         Hao Yin <haoyin@uw.edu>
+ * Author: Oliver Renaldi <oliver.renaldi@rwth-aachen.de>
  */
 
 #pragma once
